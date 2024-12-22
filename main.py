a6b06eda47ff137793cc3b8bca61b92878167ee5
lafoxo <info4lolo6@gmail.com>
2024-12-22 18:04:17 +0100

    commit

---
+++ main.py
@@ -0,0 +1,8 @@
+i =int(input("enter a number:"))
+if (i<15):
+   print("i is smaller than 15")
+   print("im in if block")
+else:
+   print ("i is greater than 15")
+   print("im in else block")
+print("im not in if and not in else ")   
